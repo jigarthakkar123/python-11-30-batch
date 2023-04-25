c53696a2aa4f78b84001f4f4b5e97b7e761d7b6b
jigarthakkar123 <jigar.thakkar.tops@gmail.com>
2023-04-25 12:43:47 +0530

    Third Commit

---
+++ StringSlice.py
@@ -0,0 +1,15 @@
+#  01234567890123456    +
+s="ABC DE FGHI JK LM"
+#  76543210987654321    -
+
+print(s[3:13])
+print(s[:15])
+print(s[5:])
+print(s[2:15:3])
+print(s[::5])
+
+print(s[-15:-7])
+print(s[:-2])
+print(s[-17:])
+print(s[-15:-3:4])
+print(s[::-3])
